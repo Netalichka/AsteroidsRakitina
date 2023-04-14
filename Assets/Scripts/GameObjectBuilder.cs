9e70b8985e1386d422ad720bc35afc56f86095cf
Netalichka <netalichka2@gmail.com>
2023-04-14 18:05:24 +0300

    correctmistakes

--- Assets/Scripts/GameObjectBuilder.cs
+++ Assets/Scripts/GameObjectBuilder.cs
@@ -10,7 +10,7 @@ namespace Asteroids.Builder
         protected GameObject _gameObject;
         public GameObjectBuilder() => _gameObject = new GameObject();
         protected GameObjectBuilder(GameObject gameObject) => _gameObject = gameObject;
-        public GameObjectVisualBuilder Visual => new GameObjectVisualBuilder(gameObject);
+        public GameObjectVisualBuilder Visual => new GameObjectVisualBuilder(_gameObject);
         public GameObjectPhysicsBuilder Physics => new GameObjectPhysicsBuilder(_gameObject);
         public static implicit operator GameObject(GameObjectBuilder builder)
         {
